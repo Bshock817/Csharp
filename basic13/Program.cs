49bba0af31abf0867d49210f28f25711bb282abb
Brian Shockency <Bshock817@gmail.com>
2020-07-27 15:18:20 -0400

    13 functions practice

---
+++ basic13/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace basic13
+{
+    class Program
+    {
+        // public static void PrintNumArray(){
+        // for(int i = 1; i<256; i++){
+        // Console.WriteLine(i);
+        // }}
+
+        // public static void PrintOdds(){
+        //     for(var i =0; i<256;i++){
+        //         if(i%2 != 0){
+        //             Console.WriteLine(i);
+        //         }
+        //     }
+        // }
+
+        // public static void PrintSum(){
+        //     int sum = 0;
+        //     for(var i = 0; i<256;i++){
+        //         sum = sum +=i;
+        //         Console.WriteLine($"Old Number: {i} - New Number: {sum}");
+        //     }
+        // }
+
+        // public static void LoopArray(int[] numbers){
+        //     foreach(var num in numbers){
+        //         Console.WriteLine(num);
+        //     }
+        // }
+
+        // public static int FindMax(int[] numbers)
+        // {
+        //     int currMax = Int32.MinValue;
+        //     foreach(int num in numbers)
+        //     {
+        //         if(currMax < num)
+        //             currMax = num;
+        //     }
+        //     Console.WriteLine($"Max is {currMax}");
+        //     return currMax;
+        // }
+        
+        // public static float GetAverage(int[] numbers)
+        // {
+        //     int sum = 0;
+        //     foreach(int num in numbers)
+        //         sum += num;
+
+        //     float avg = (float)sum / numbers.Length;
+        //     Console.WriteLine($"Average is {avg}");
+        //     return avg;
+        // }
+
+        // public static int[] OddArray()
+        // {
+        //     // first we must find size of array
+        //     int size = (255/2) + 1;
+        //     int[] odds = new int[size];
+
+        //     int i = 0;
+        //     for(int num = 1; num <= 255; num+=2)
+        //     {
+        //         odds[i] = num;
+        //         i++;
+        //     }
+        //     return odds;
+        // }
+
+        // public static int GreaterThanY(int[] numbers, int y)
+        // {
+        //     int total = 0;
+        //     for(int i = 0; i < numbers.Length; i++)
+        //     {
+        //         if(numbers[i] > y)
+        //             total++;
+        //     }
+        //     return total;
+        // }
+
+        // public static void SquareArrayValues(int[] numbers)
+        // {
+        //     for(int i = 0; i < numbers.Length; i++)
+        //     {
+        //         numbers[i] = numbers[i] * numbers[i];
+        //     }
+        // }
+
+        // public static void EliminateNegatives(int[] numbers)
+        // {
+        //     for(int i = 0; i < numbers.Length; i++)
+        //     {
+        //         if(numbers[i] < 0)
+        //             numbers[i] = 0;
+        //     }
+        // }
+
+        // public static void MinMaxAverage(int[] numbers)
+        // {
+        //     int currMin = Int32.MaxValue,
+        //         currMax = Int32.MinValue,
+        //         sum = 0;
+            
+        //     foreach(int num in numbers)
+        //     {
+        //         sum += num;
+        //         if(currMax < num)
+        //             currMax = num;
+        //         if(currMin > num)
+        //             currMin = num;
+        //     }
+        //     Console.WriteLine($"Min: {currMin}");
+        //     Console.WriteLine($"Max: {currMax}");
+        //     Console.WriteLine($"Average: {(float)sum / numbers.Length}");
+        // }
+
+        // public static void ShiftArrayValues(int[] numbers)
+        // {
+        //     for(int i = 0; i < numbers.Length - 1; i++)
+        //     {
+        //         numbers[i] = numbers[i + 1];
+        //     }
+        //     numbers[numbers.Length - 1] = 0;
+        // }
+
+        // public static object[] NumToString(int[] numbers)
+        // {
+        //     object[] newArr = new object[numbers.Length];
+        //     for(int i = 0; i < numbers.Length; i++)
+        //     {
+        //         if(numbers[i] < 0)
+        //             newArr[i] = "Dojo";
+        //         else
+        //             newArr[i] = numbers[i];
+        //     }
+        //     return newArr;
+        // }
+        static void Main(string[] args)
+        {
+            // PrintNumArray();
+            // PrintOdds();
+            // PrintSum();
+            // LoopArray();
+            // FindMax();
+            // GetAverage();
+            // OddArray();
+            // GreaterThanY();
+            // SquareArrayValues();
+            // EliminateNegatives();
+            // MinMaxAverage();
+            // ShiftArrayValues();
+            // NumToString();
+        }
+    }
+}
